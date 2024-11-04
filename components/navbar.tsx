9d59dadd9e05f615c56fcfdd80123377577f8966
Shahriar-Samir <shabusiness035@gmail.com>
2024-11-04 23:15:44 +0600

    update: navbar design system

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,56 +1,33 @@
-import {
-  Navbar as NextUINavbar,
-  NavbarContent,
-  NavbarMenu,
-  NavbarMenuToggle,
-  NavbarBrand,
-  NavbarItem,
-  NavbarMenuItem,
-} from "@nextui-org/navbar";
-import { Button } from "@nextui-org/button";
-import { Kbd } from "@nextui-org/kbd";
-import { Link } from "@nextui-org/link";
-import { Input } from "@nextui-org/input";
-import { link as linkStyles } from "@nextui-org/theme";
 import NextLink from "next/link";
-import clsx from "clsx";
-
-import { siteConfig } from "@/config/site";
-import { ThemeSwitch } from "@/components/theme-switch";
-import {
-  GithubIcon,
-  HeartFilledIcon,
-  SearchIcon,
-  Logo,
-} from "@/components/icons";
 import Image from "next/image";
 
 export const Navbar = () => {
  
 
   return (
-    <NextUINavbar className="bg-gradient-to-b from-[#EEE0FF] to-[#fcf9ff] py-4 px-16 "  maxWidth="xl" position="sticky" >
-      <NavbarContent className="justify-center" data-justify="center">
-        <NavbarBrand as="li" className="gap-3 max-w-fit">
-          <NextLink className="flex justify-start items-center gap-1" href="/">
-            <Image width={90} height={30} alt={'logo'} src="/primary-logo.svg"/>
-          </NextLink>
-        </NavbarBrand>
-      <div>
-        <h1 className="font-font2 font-bold text-xl">Delivery in 
-        <span className="font-font5 text-lg"> 7 Mins</span></h1>
-        <button className="text-sm font-semibold font-font3">Pocket 25, Subhash Place, Rohini, Delhi, Delhi</button>
-      </div>
-      </NavbarContent>
 
-      <NavbarContent
-        className="hidden sm:flex w-full gap-5"
+      <nav className="flex flex-col md:flex-row justify-between md:items-center bg-[#D1ABFF] md:bg-gradient-to-b md:from-[#EEE0FF] md:to-[#fcf9ff] px-2 py-4 md:px-16 gap-5">
+      <div className="flex gap-5 items-center">
+        <div className="gap-3">
+          <NextLink className="flex justify-start items-center gap-1" href="/">
+            <Image width={90} height={30} alt={'logo'} src="/primary-logo.svg" className="hidden md:inline min-w-[90px] min-h-[30px]"/>
+          </NextLink>
+        </div>
+      <div>
+        <h1 className="font-font2 font-bold text-lg w-max">Delivery in 
+        <span className="font-font5 "> 7 Mins</span></h1>
+        <button className="text-sm font-semibold font-font3 w-max">Pocket 25, Subhash Placesdf dsasdf sd</button>
+      </div>
+      </div>
+
+      <div
+        className="flex w-full gap-5"
       >
-        <NavbarItem className="hidden lg:flex w-full bg-[#FFFFFF] rounded-lg py-3 px-4 gap-5">
-          <Image height={50} width={50} alt="search icon" src="/search-icon.svg" className="h-[20px] w-[20px]"/>         
+        <div className="flex w-full bg-[#FFFFFF] rounded-lg py-3 px-4 gap-5">
+          <Image height={50} width={50} alt="search icon" src="/search-icon.svg" className="h-[20px] w-[20px] hidden md:inline"/>         
           <p className="font-font1">Search for </p>
-        </NavbarItem>
-        <NavbarItem className="hidden md:flex gap-5">
+        </div>
+        <div className="hidden md:flex gap-5">
           <button className="flex flex-col items-center">
             <Image height={24} width={24} alt="user" src="/icons/user.svg"/>
             <p className="text-xs">Login</p>
@@ -59,38 +36,11 @@ export const Navbar = () => {
             <Image height={24} width={24} alt="user" src="/icons/cart.svg"/>
             <p className="text-xs">Cart</p>
           </button>
-        </NavbarItem>
-      </NavbarContent>
-
-      <NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
-        <Link isExternal aria-label="Github" href={siteConfig.links.github}>
-          <GithubIcon className="text-default-500" />
-        </Link>
-        <ThemeSwitch />
-        <NavbarMenuToggle />
-      </NavbarContent>
-
-      <NavbarMenu>
-        <div className="mx-4 mt-2 flex flex-col gap-2">
-          {siteConfig.navMenuItems.map((item, index) => (
-            <NavbarMenuItem key={`${item}-${index}`}>
-              <Link
-                color={
-                  index === 2
-                    ? "primary"
-                    : index === siteConfig.navMenuItems.length - 1
-                      ? "danger"
-                      : "foreground"
-                }
-                href="#"
-                size="lg"
-              >
-                {item.label}
-              </Link>
-            </NavbarMenuItem>
-          ))}
         </div>
-      </NavbarMenu>
-    </NextUINavbar>
+      </div>
+
+      </nav>
+
+
   );
 };
