f86557fb2a03a85eb0163e3aa56edce34ea614d6
Shahriar-Samir <shabusiness035@gmail.com>
2024-11-07 12:16:57 +0600

    add: snacks & drinks section on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,35 @@ import Image from "next/image";
 
 
 export default function Home() {
+
+  const images:string[]=[
+    '/category/1.webp',
+    '/category/2.webp',
+    '/category/3.webp',
+    '/category/4.webp',
+    '/category/5.webp',
+    '/category/6.webp',
+    '/category/7.webp',
+  ]
+  
   return (
-    <section className="flex flex-col items-center justify-center ">
+    <main className="px-6">
         {/* <Image src='/paan-corner-banner.webp' width={2000} height={300} alt="w-full h-full paan-corner-banner" className="px-4 "/> */}
-    </section>
+        <h1 className="text-xl font-font4">Snacks & Drinks</h1>
+        <section className="grid grid-cols-8 gap-5 mt-6">
+          {images.map((item,index)=>{
+                if(index===0){
+                  return <div key={item} className="col-span-2 flex flex-col items-center gap-2 ">
+                <Image width={300} height={300} alt="example" src={item}/>
+            </div>
+                }
+                else{
+                  return <div key={item} className="flex flex-col items-center gap-2 ">
+                <Image width={300} height={300} alt="example" src={item}/>
+            </div>
+                }
+          })}
+        </section>
+    </main>
   );
 }
